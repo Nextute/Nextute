7cd383adda984f7f2ae5df5a515a18379668916d
Kamlesh Prajapati <kamleshprajapati0928@gmail.com>
2025-06-21 15:57:37 +0530

    remove all codes

--- utils/errorHandler.js
+++
@@ -1,7 +0,0 @@
-const handleError = (res, status, message, errorCode = null) => {
-  const response = { message };
-  if (errorCode) response.errorCode = errorCode;
-  res.status(status).json(response);
-};
-
-export { handleError };
