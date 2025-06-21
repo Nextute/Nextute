1546fb9e89310a90b8968883282eff733ed978b5
Kamlesh Prajapati <kamleshprajapati0928@gmail.com>
2025-06-21 20:20:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 // Security middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
